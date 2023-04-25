7e5543807bc9d6c7e35d0eb3d0b504e7544a78d9
plphuc <phamluumyphuc@gmail.com>
2023-04-25 17:03:36 +0700

    change column names + edit st main_3.py

--- main_3.py
+++ main_3.py
@@ -16,7 +16,7 @@ import sys
 
 data_path = "./data/yelp_min/"
 train_name = "yelp_min_train.csv"
-test_name = "yelp-min_test.csv"
+test_name = "yelp_min_test.csv"
 model_save_path = sys.argv[1]
 
 
@@ -51,7 +51,7 @@ TEXT = data.Field(batch_first=True,
                   unk_token = unk_token_idx)
 
 LABEL = data.LabelField()
-fields = [('text',TEXT),('sentiment',LABEL)]
+fields = [('text',TEXT),('stars',LABEL)]
 
 train_data, test_data = data.TabularDataset.splits(
                             path = data_path,
