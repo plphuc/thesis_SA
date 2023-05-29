2057ee259a815d8c2683c325edbaf34c5d2ccbbb
Nguyen Doan Phuc <68143637+doanphuc281201@users.noreply.github.com>
2023-05-29 22:58:05 +0700

    Update main_VLSP.py

--- main_VLSP.py
+++ main_VLSP.py
@@ -23,9 +23,9 @@ from sklearn.metrics import f1_score
 from transformers import PhobertTokenizer, AutoTokenizer, XLMRobertaTokenizer, RobertaForSequenceClassification
 import sys
 
-data_path = "./data/VLSP"
-train_name = "VLSP_train.csv"
-test_name = "VLSP_test.csv"
+data_path = "./data/VLSP_copy"
+train_name = "VLSP_train.tsv"
+test_name = "VLSP_test.tsv"
 
 model_save_path = sys.argv[1]
 
